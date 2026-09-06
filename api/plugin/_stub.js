618ba88daa67bbd17f47e1e2a3b97a42f74b10b9
DIPIPb <sanfranciskobogdan@gmail.com>
2026-09-06 23:49:42 +0300

    Delete api directory

--- api/plugin/_stub.js
+++
@@ -1,9 +0,0 @@
-const { requirePluginAuth } = require('../../lib/pluginAuth');
-
-// Accepts any method/body, just checks auth and returns ok.
-// Wire this up from a route file with: module.exports = require('../_stub');
-module.exports = async (req, res) => {
-  const server = await requirePluginAuth(req, res);
-  if (!server) return;
-  res.status(200).json({ ok: true });
-};
--- api/plugin/index.js
+++
@@ -1,8 +0,0 @@
-const { requirePluginAuth } = require('../../lib/pluginAuth');
-
-module.exports = async (req, res) => {
-  if (req.method !== 'GET') return res.status(405).json({ ok: false, error: 'method_not_allowed' });
-  const server = await requirePluginAuth(req, res);
-  if (!server) return;
-  res.status(200).json({ ok: true, server_id: server.id });
-};
